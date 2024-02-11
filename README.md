c9da2d8a35b39465752da6bb50f932fd7b8c432c
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2024-02-11 12:50:38 +0530

    chore: make connect to the database

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pocket Memory: A gallery App
+
+## Copyright
+
+Injamul Mohammad Mollah (c) 2024 mrinjamul(at)gmail(dot).com
--- app.js
+++ app.js
@@ -4,10 +4,22 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
+
+// Get Service config and environments
+const config = require("./config").getConfig();
+
+// Connect to the database
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(config.database.url, {
+    dbName: config.database.name,
+  })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => console.log(err));
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,7 +28,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors()); // cors for cross origin access
 
+// Import routers
+
+var indexRouter = require("./routes/index");
+// var authRouter = require("./routes/auth");
+
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/auth", authRouter);
 
 module.exports = app;
