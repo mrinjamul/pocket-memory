20ad5bd7727545830d5883f6bed15f19d7223dfe
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2024-02-20 22:50:57 +0530

    ui: fetch random picture in landing page

--- ui/src/pages/Landing.jsx
+++ ui/src/pages/Landing.jsx
@@ -6,7 +6,7 @@ import Hero from "../components/Hero";
 import Gallery from "../components/Gallery";
 
 const LandingPage = () => {
-  const [gallery, setGallery] = useState([{}]);
+  const [gallery, setGallery] = useState([]);
 
   const apiURL = import.meta.env.VITE_APP_APIURL || "http://localhost:4000";
 
@@ -21,15 +21,29 @@ const LandingPage = () => {
     }
   };
 
+  // Function to shuffle array
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  // Function to get random 8 pictures from the gallery
+  const getRandomPictures = () => {
+    const shuffledGallery = shuffleArray([...gallery]); // Shuffle the gallery array
+    return shuffledGallery.slice(0, 8); // Return the first 8 elements
+  };
+
   useEffect(() => {
-    getPhotos();
-    return () => {};
+    getPhotos(); // Fetch photos when the component mounts
   }, []);
 
   return (
     <BaseLayout>
       <Hero />
-      <Gallery userPhotos={gallery} />
+      <Gallery userPhotos={getRandomPictures()} />
     </BaseLayout>
   );
 };
